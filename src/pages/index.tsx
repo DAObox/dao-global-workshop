14ff0a6a090a0b8477967ba7348327f110fdf256
pythonpete32 <aaron@blackscale.io>
2023-04-02 00:02:03 +0400

    add layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,15 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { useAccount } from "wagmi";
-import { Card, Metric, Text, Flex, BadgeDelta, Grid } from "@tremor/react";
+import {
+  Card,
+  Metric,
+  Text,
+  Flex,
+  BadgeDelta,
+  Grid,
+  Title,
+  Col,
+} from "@tremor/react";
 
 import {
   useFetchDao,
@@ -16,9 +25,9 @@ import { daoAddress, votingPluginAddress } from "../constants";
 function Page() {
   const { isConnected } = useAccount();
   const dao = useFetchDao({ daoAddressOrEns: daoAddress });
+  const members = useFetchMembers({ pluginAddress: votingPluginAddress });
 
   // Open the devtools to see the data
-  useFetchMembers({ pluginAddress: votingPluginAddress });
   useFetchProposals({ daoAddressOrEns: votingPluginAddress });
   useFetchTransfers({ daoAddressOrEns: daoAddress });
   useFetchDaoBalances({ daoAddressOrEns: daoAddress });
@@ -28,10 +37,40 @@ function Page() {
   return (
     <>
       <Header />
-      <div className="p-4 px-6 mx-auto">
-        <StatsBar />
-        {dao && <pre>{JSON.stringify(dao.data, null, 2)}</pre>}
-      </div>
+      <main className="p-4 px-6 mx-auto">
+        <Title>Dashboard</Title>
+        <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr.</Text>
+
+        <Grid numColsLg={6} className="gap-6 mt-6">
+          {/* Main section */}
+          <Col numColSpanLg={4}>
+            <Card className="h-full overflow-auto  p-4">
+              {dao && <pre>{JSON.stringify(dao.data, null, 2)}</pre>}
+            </Card>
+          </Col>
+
+          {/* KPI sidebar */}
+          <Col numColSpanLg={2}>
+            <div className="space-y-6">
+              <StatCard
+                title="Members"
+                metric={members?.data?.length}
+                delta={10}
+              />
+              <StatCard
+                title="Members"
+                metric={members?.data?.length}
+                delta={-40}
+              />
+              <StatCard
+                title="Members"
+                metric={members?.data?.length}
+                delta={70}
+              />
+            </div>
+          </Col>
+        </Grid>
+      </main>
     </>
   );
 }
@@ -43,7 +82,7 @@ function Header() {
     <header className="w-full  p-4">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
-          <div className=" font-bold text-xl">DAO Box</div>
+          <div className=" font-bold text-xl"></div>
           <ConnectButton />
         </div>
       </div>
@@ -51,36 +90,22 @@ function Header() {
   );
 }
 
-function StatsBar() {
-  const members = useFetchMembers({ pluginAddress: votingPluginAddress });
-
-  return (
-    <Grid numColsSm={2} numColsLg={3} className="gap-6">
-      {members && (
-        <>
-          <StatCard title="Members" metric={members?.data?.length} delta={50} />
-          <StatCard title="Members" metric={members?.data?.length} delta={30} />
-          <StatCard title="Members" metric={members?.data?.length} delta={10} />
-        </>
-      )}
-    </Grid>
-  );
-}
-
 function StatCard({
   title,
   metric,
   delta,
+  deltaType,
 }: {
   title: string;
   metric?: number;
+  deltaType?: "increase" | "decrease";
   delta?: number;
 }) {
   return (
     <Card>
       <Flex alignItems="start">
         <Text>{title}</Text>
-        {delta && <BadgeDelta deltaType="increase">{delta}%</BadgeDelta>}
+        {delta && <BadgeDelta deltaType={deltaType}>{delta}%</BadgeDelta>}
       </Flex>
       <Flex
         justifyContent="start"
