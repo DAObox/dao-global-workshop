4fd1614e30cb36990f8698ebc5526467c94c18b6
Victor Omorogbe <victoromorogbe69@gmail.com>
2023-04-02 21:25:42 +0100

    suprememods

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -6,7 +6,7 @@ export default function Badge({ active }: Props) {
     return(
         <span className={`inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium 
             ${active ? "bg-green-500/40 text-green-700" : "bg-orange-500/40 text-orange-700"}`}>
-            Active
+            { active ? "Active" : "Inactive" }
         </span>
     )
 }
