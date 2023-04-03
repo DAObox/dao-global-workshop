7eee5e4e34b23f76fdafa420ee05c39d52b70492
pythonpete32 <aaron@blackscale.io>
2023-04-03 21:28:07 +0400

    always visible and has connect button

--- src/components/layout/Headbar.tsx
+++ src/components/layout/Headbar.tsx
@@ -1,27 +1,26 @@
 import { Bars3Icon } from "@heroicons/react/24/outline";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
-export function Headbar(props) {
+export function Headbar({ setSidebarOpen }: { setSidebarOpen: any }) {
   return (
-    <div className="sticky top-0 z-40 flex items-center px-4 py-4 bg-white shadow-sm gap-x-6 sm:px-6 lg:hidden">
+    <div className="sticky top-0 z-40 flex h-16 shrink-0 items-center gap-x-4 border-b border-gray-200 bg-white px-4 shadow-sm sm:gap-x-6 sm:px-6 lg:px-8 lg:pl-72">
       <button
         type="button"
         className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-        onClick={() => props.setSidebarOpen(true)}
+        onClick={() => setSidebarOpen(true)}
       >
         <span className="sr-only">Open sidebar</span>
-        <Bars3Icon className="w-6 h-6" aria-hidden="true" />
+        <Bars3Icon className="h-6 w-6" aria-hidden="true" />
       </button>
-      <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
-        Dashboard
+
+      <div className="h-6 w-px bg-gray-900/10 lg:hidden" aria-hidden="true" />
+
+      <div className="flex flex-1 gap-x-4 self-stretch lg:gap-x-6">
+        <div className="relative flex flex-1" />
+        <div className="flex items-center gap-x-4 lg:gap-x-6">
+          <ConnectButton />
+        </div>
       </div>
-      <a href="#">
-        <span className="sr-only">Your profile</span>
-        <img
-          className="w-8 h-8 rounded-full bg-gray-50"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          alt=""
-        />
-      </a>
     </div>
   );
 }
